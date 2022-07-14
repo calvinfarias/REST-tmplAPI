bcb8564c587d0dfb2fb67ee4df4687215b10d6c5
Calvin Farias <calvin.farias@ufrgs.br>
2022-07-14 04:09:07 -0300

    Update

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='RESTAPI1stTMPL',
+    version='0.1',
+    url="https://github.com/calvinfarias/RESTAPI1stTMPL",
+    description="A simple REST API that implements a request header's modification",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: Linux",
+    ],
+)
+
