38ea88e15c7ddcddf5c6c3dc657422bfa6719d43
Calvin Farias <calvin.farias@ufrgs.br>
2022-07-14 21:35:16 -0300

    Update

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setuptools.setup(
     description="A simple REST API that implements a a request header's modification template",
     url="https://github.com/calvinfarias/REST-tmplAPI",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'fastapi',
+        'uvicorn',
+        'pytest',
+        'requests'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
